06aebad6f4217721efd7f12513293f693f740806
IulianaJ <iuliana.jaba@gmail.com>
2022-06-09 14:30:08 +0300

    Added data for Card & CardList components

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -1,7 +1,16 @@
+import React from "react";
 import "./App.css";
+import CardList from "./components/CardList";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
+        <CardList />
+      </div>
+    );
+  }
 }
 
 export default App;
