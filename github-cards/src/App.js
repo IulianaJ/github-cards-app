63401ee2f51f455725b683867dd21c05241cca66
IulianaJ <iuliana.jaba@gmail.com>
2022-06-09 16:52:40 +0300

    Updated components

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -3,34 +3,20 @@ import "./App.css";
 import CardList from "./components/CardList";
 import Form from "./components/Form";
 
-const testData = [
-  {
-    name: "Dan Abramov",
-    avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4",
-    company: "Facebook",
-  },
-  {
-    name: "Sophie Alpert",
-    avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4",
-    company: "Humu",
-  },
-  {
-    name: "Sebastian Markbåge",
-    avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4",
-    company: "Facebook",
-  },
-];
-
 class App extends React.Component {
   state = {
-    profiles: testData,
+    profiles: [],
   };
-
+  addNewProfile = (profileData) => {
+    this.setState((prevState) => ({
+      profiles: [...prevState.profiles, profileData],
+    }));
+  };
   render() {
     return (
       <div>
         <div className="header">{this.props.title}</div>
-        <Form />
+        <Form onSubmit={this.addNewProfile} />
         <CardList profiles={this.state.profiles} />
       </div>
     );
